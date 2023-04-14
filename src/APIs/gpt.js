ab70ad5301b28b0f5818787439e9916b9b587a71
Ryan Gibson <gibson.ryan2015@gmail.com>
2023-04-14 00:53:22 -0600

    setting up lambda functions

---
+++ src/APIs/gpt.js
@@ -0,0 +1,27 @@
+// openaiHelper.js
+import openai from "openai";
+
+const API_KEY = "your_openai_api_key";
+openai.apiKey = API_KEY;
+
+export const fetchOpenAIResponse = async (prompt) => {
+  try {
+    const response = await openai.Completion.create({
+      engine: "davinci-codex",
+      prompt: prompt,
+      max_tokens: 4096,
+      n: 1,
+      stop: null,
+      temperature: 0.5,
+    });
+
+    if (response.choices && response.choices.length > 0) {
+      return response.choices[0].text.trim();
+    } else {
+      throw new Error("No response received from the OpenAI API");
+    }
+  } catch (error) {
+    console.error("Error fetching OpenAI API response:", error);
+    return null;
+  }
+};
